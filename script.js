448d6bec01e82ef5d96a9883a20e251c5ce4fc20
pc <pc@pcs-MacBook-Pro.local>
2023-03-29 14:11:28 +0400

    added final changes in the project

--- script.js
+++ script.js
@@ -31,8 +31,7 @@ let myArray = [];
 
 displayJobs(data);
 
-function displayJobs(arr){
-
+function displayJobs(arr) {
   jobCard.innerHTML = "";
 
   for (let i = 0; i < arr.length; i++) {
@@ -50,40 +49,44 @@ function displayJobs(arr){
       languages,
       tools,
     } = arr[i];
-  
+
     const vacancyCard = elementCreator("div", "job-card");
 
     const asideDesignDiv = elementCreator("div", "aside-design-card");
-  
+
     const innerCardOne = elementCreator("div", "inner-card-one", null);
     const innerCardTwo = elementCreator("div", "inner-card-two", null);
-  
+
     const imgContainer = elementCreator("div", "image-container");
     const logoElement = elementCreator("img", "image", logo);
-  
+
     const companyName = elementCreator("div", "company-name");
     const companyTitle = elementCreator("h2", "company-title", null, company);
-  
+
     const positionDiv = elementCreator("div", "position-div", null);
     const positionTitle = elementCreator("p", "position-name", null, position);
-  
+
     const timeNlocation = elementCreator("div", "time-and-location");
     const postedTime = elementCreator("p", "posted-time", null, postedAt);
     const contractType = elementCreator("li", "contract-type", null, contract);
     const jobLocation = elementCreator("li", "job-location", null, location);
-  
+
     const horizonalLine = elementCreator("div", "horizonal-line", null);
-  
-    const additionalFeatures = elementCreator("div", "additional-features", null);
+
+    const additionalFeatures = elementCreator(
+      "div",
+      "additional-features",
+      null
+    );
     const roleRangeings = elementCreator("p", "role-rangeings", null, role);
     const positionLevel = elementCreator("p", "position-level", null, level);
-  
+
     const proggramingLanguages = elementCreator(
       "div",
       "proggraming-languages",
       null
     );
-  
+
     for (let j = 0; j < arr[i].languages.length; j++) {
       let programmingLangEl = elementCreator(
         "p",
@@ -92,24 +95,34 @@ function displayJobs(arr){
         languages
       );
       proggramingLanguages.append(programmingLangEl);
-  
+
       programmingLangEl.textContent = arr[i].languages[j];
     }
-  
+
     for (let j = 0; j < arr[i].tools.length; j++) {
-      let additionalTools = elementCreator("p", "additional-tools", null, tools);
+      let additionalTools = elementCreator(
+        "p",
+        "additional-tools",
+        null,
+        tools
+      );
       proggramingLanguages.append(additionalTools);
-  
+
       additionalTools.textContent = arr[i].tools[j];
     }
-  
+
     const newlyAded = elementCreator("p", "new", null, statusNew);
     newlyAded.textContent = "NEW!";
-  
+
     const feature = elementCreator("p", "featured", null, featured);
     feature.textContent = "FEATURED";
-  
-    vacancyCard.append(asideDesignDiv, imgContainer, innerCardOne, innerCardTwo);
+
+    vacancyCard.append(
+      asideDesignDiv,
+      imgContainer,
+      innerCardOne,
+      innerCardTwo
+    );
     innerCardOne.append(
       companyName,
       positionDiv,
@@ -117,59 +130,54 @@ function displayJobs(arr){
       horizonalLine,
       additionalFeatures
     );
-  
+
     positionDiv.append(positionTitle);
     companyName.append(companyTitle);
     jobCard.append(vacancyCard);
 
-  
     timeNlocation.append(postedTime, contractType, jobLocation);
     imgContainer.append(logoElement);
-  
+
     innerCardTwo.append(additionalFeatures);
-    additionalFeatures.append(roleRangeings, positionLevel, proggramingLanguages);
-  
+    additionalFeatures.append(
+      roleRangeings,
+      positionLevel,
+      proggramingLanguages
+    );
+
     if (statusNew) {
       companyName.append(newlyAded);
     }
-  
+
     if (featured) {
       companyName.append(feature);
     }
+
+    let thirdChild = companyName.childNodes[2];
+
+    if (thirdChild) {
+      asideDesignDiv.style.display = "block";
+    } else {
+      asideDesignDiv.style.display = "none";
+    }
   }
 
+  let vacanyRole = document.querySelectorAll(".role-rangeings");
+  let position = document.querySelectorAll(".position-level");
+  let proggramingLanguages = document.querySelectorAll(".programming-lang-el");
+  let aditionalToolsFilterEl = document.querySelectorAll(".additional-tools");
 
-let vacanyRole = document.querySelectorAll(".role-rangeings");
-let position = document.querySelectorAll(".position-level");
-let proggramingLanguages = document.querySelectorAll(".programming-lang-el");
-let aditionalToolsFilterEl = document.querySelectorAll(".additional-tools");
+  vacanyRole.forEach(function (val) {
+    val.addEventListener("click", function () {
+      filterCard.style.display = "block";
+      if (!myArray.includes(val.textContent)) {
+        myArray.push(val.textContent);
+      }
+      filterOutputFunc();
 
-
-
-vacanyRole.forEach(function (val) {
-  val.addEventListener("click", function () {
-    filterCard.style.display = "block";
-    if (!myArray.includes(val.textContent)) {
-      myArray.push(val.textContent);
-    }
-    filterOutputFunc();
-
-    arrayFilter();
-
-    // let newArray = data.filter(
-    //   (el) =>
-    //   myArray.every(item => 
-    //     el.role === item ||
-    //     el.level === item ||
-    //     el.languages.includes(item) ||
-    //     el.tools.includes(item)
-    //   )
-
-    // );
-    // displayJobs(newArray);
-
+      arrayFilter();
+    });
   });
-});
 
   position.forEach(function (val) {
     val.addEventListener("click", function () {
@@ -178,26 +186,11 @@ vacanyRole.forEach(function (val) {
         myArray.push(val.textContent);
       }
       filterOutputFunc();
-  
+
       arrayFilter();
-      // let newArray = data.filter(
-      //   (el) =>
-      //   myArray.every(item => 
-      //     el.role === item ||
-      //     el.level === item ||
-      //     el.languages.includes(item) ||
-      //     el.tools.includes(item)
-     
-      //   )
-          
-      // );
-      // displayJobs(newArray);
     });
   });
 
-
-
-  
   proggramingLanguages.forEach(function (val) {
     val.addEventListener("click", function () {
       filterCard.style.display = "block";
@@ -207,25 +200,9 @@ vacanyRole.forEach(function (val) {
       filterOutputFunc();
 
       arrayFilter();
-
-      // let newArray = data.filter(
-      //   (el) =>
-      //   myArray.every(item => 
-      //     el.role === item ||
-      //     el.level === item ||
-      //     el.languages.includes(item) ||
-      //     el.tools.includes(item)
-   
-      //   )
-          
-      // );
-      // displayJobs(newArray);
-
     });
   });
-  
-  
-  
+
   aditionalToolsFilterEl.forEach(function (val) {
     val.addEventListener("click", function () {
       filterCard.style.display = "block";
@@ -233,33 +210,24 @@ vacanyRole.forEach(function (val) {
         myArray.push(val.textContent);
       }
       filterOutputFunc();
-    
+
       arrayFilter();
-
-
-
-
     });
   });
-
-
 }
 
-function arrayFilter(){
-    let newArray = data.filter(
-        (el) =>
-        myArray.every(item => 
-          el.role === item ||
-          el.level === item ||
-          el.languages.includes(item) ||
-          el.tools.includes(item)
-        )  
-      );
-      displayJobs(newArray);
+function arrayFilter() {
+  let newArray = data.filter((el) =>
+    myArray.every(
+      (item) =>
+        el.role === item ||
+        el.level === item ||
+        el.languages.includes(item) ||
+        el.tools.includes(item)
+    )
+  );
+  displayJobs(newArray);
 }
-
-
-
 
 function filterOutputFunc() {
   choosenSection.innerHTML = "";
@@ -307,8 +275,6 @@ function filterOutputFunc() {
   });
 }
 
-
-
 const removeBtn = document.getElementById("btn");
 removeBtn.addEventListener("click", resetEverything);
 
@@ -316,4 +282,5 @@ function resetEverything() {
   myArray = [];
   filterOutputFunc();
   filterCard.style.display = "none";
+  arrayFilter();
 }
